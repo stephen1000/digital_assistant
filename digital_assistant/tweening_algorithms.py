d8021ab7e960fd6acb98c3d43c8130ccc04cfeb4
Stephen Lowery <stephen.e.lowery@gmail.com>
2019-10-30 19:04:44 -0400

    added random step tweener

--- digital_assistant/tweening_algorithms.py
+++ digital_assistant/tweening_algorithms.py
@@ -1,10 +1,12 @@
+""" Algorithms used for tweening values """
+
+import random
 from abc import ABC
 
 # assume val is between 0 and 1
 # return pos between 0 and 1
 MAX_POS = 1
 MIN_POS = 0
-
 
 
 class TweeningAlgorithm(ABC):
@@ -14,36 +16,37 @@ class TweeningAlgorithm(ABC):
     min_pos = MIN_POS
     max_pos = MAX_POS
 
-    def set_min(self, val:float):
+    def set_min(self, val: float):
         """ Sets ``min_pos`` to ``val``"""
         self.min_pos = val
 
-    def set_max(self, val:float):
+    def set_max(self, val: float):
         """ Sets ``max_pos`` to ``val``"""
         self.max_pos = val
 
-    def set_min_max(self, lo:float, hi:float):
+    def set_min_max(self, lo: float, hi: float):
         """ Sets ``min_pos`` to ``lo`` and ``max_pos`` to ``hi``"""
         self.min = lo
         self.max = hi
 
-    def _calc(self, t:float) -> float:
+    def _calc(self, t: float) -> float:
         """ Implement this as a way to find the current position as a function of time """
         raise NotImplementedError
 
-    def get_pos(self, t:float, **kwargs) -> float:
+    def get_pos(self, t: float, **kwargs) -> float:
         pos = self._calc(t, **kwargs)
         if pos > self.max_pos:
             return self.max_pos
         return pos
 
+
 class TweenLinear(TweeningAlgorithm):
     """ Linear algorithm, given a slope (default 1)"""
 
-    def __init__(self, slope:float=1.0):
+    def __init__(self, slope: float = 1.0):
         self.slope = slope
 
-    def _calc(self, t:float, slope:float=None):
+    def _calc(self, t: float, slope: float = None):
         """ Calculate position linearally """
         if slope is None:
             slope = self.slope
@@ -52,12 +55,26 @@ class TweenLinear(TweeningAlgorithm):
 
 class TweenQuadratic(TweeningAlgorithm):
     """ Quadratic algorithm, given a power (default 2) """
-    def __init__(self, pwr:int=2):
+
+    def __init__(self, pwr: int = 2):
         self.pwr = pwr
 
-    def _calc(self, t:float, pwr:int=None) -> float:
+    def _calc(self, t: float, pwr: int = None) -> float:
         """ Quadratic (square) algorithm """
         if pwr is None:
             pwr = self.pwr
         return t ** pwr
 
+
+
+
+class TweenRandomStep(TweeningAlgorithm):
+    """ Tween a random amount of progress forward, up to a max (default .1)"""
+
+    def __init__(self, max_step: float = 0.1):
+        self.max_step = max_step
+
+    def _calc(self, t: float, max_step: float = None) -> float:
+        """ Random step algorithm """
+        step = random.randrange(max_step * 100) / 100
+        return t + step
